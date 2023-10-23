9d9929a2192467d24aa5cf35833f03be1b8abda2
victor <au670070@uni.au.dk>
2023-10-23 10:48:52 +0200

    dagligFast fix

--- src/ordination/DagligFast.java
+++ src/ordination/DagligFast.java
@@ -2,18 +2,23 @@ package ordination;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.Arrays;
 
 public class DagligFast extends Ordination{
-    private Dosis[] dosis = new Dosis[4];
+    private Dosis[] doser = new Dosis[4];
     private double[] antalDosis;
 
     protected DagligFast(LocalDate startDato, LocalDate slutDato, double[] antalDosis) {
         super(startDato, slutDato);
         this.antalDosis = antalDosis;
-        dosis[0] = antalDosis[0]==0? null : new Dosis(LocalTime.parse("06:00"), antalDosis[0]);
-        dosis[1] = antalDosis[1]==0? null : new Dosis(LocalTime.parse("12:00"), antalDosis[1]);
-        dosis[2] = antalDosis[2]==0? null : new Dosis(LocalTime.parse("18:00"), antalDosis[2]);
-        dosis[4] = antalDosis[3]==0? null : new Dosis(LocalTime.parse("24:00"), antalDosis[3]);
+        doser[0] = antalDosis[0]==0? null : new Dosis(LocalTime.parse("06:00"), antalDosis[0]);
+        doser[1] = antalDosis[1]==0? null : new Dosis(LocalTime.parse("12:00"), antalDosis[1]);
+        doser[2] = antalDosis[2]==0? null : new Dosis(LocalTime.parse("18:00"), antalDosis[2]);
+        doser[4] = antalDosis[3]==0? null : new Dosis(LocalTime.parse("24:00"), antalDosis[3]);
+    }
+
+    public Dosis[] getDoser() {
+        return Arrays.copyOf(doser, 4);
     }
 
     @Override
