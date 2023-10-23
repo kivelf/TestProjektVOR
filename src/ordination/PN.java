73181468db009d4c7dfc4488d12f1cdaa04c9d09
Stimoroid <oliverbuus@live.dk>
2023-10-23 10:41:41 +0200

    Update PN

--- src/ordination/PN.java
+++ src/ordination/PN.java
@@ -1,9 +1,17 @@
 package ordination;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
-public class PN {
+public class PN extends Ordination {
     private final double antalEnheder;
+    private List<LocalDate> datoerForAnvendelse = new ArrayList<>();
+
+    public PN(LocalDate startDato, LocalDate slutDato, double antalEnheder) {
+        super(startDato, slutDato);
+        this.antalEnheder = antalEnheder;
+    }
 
     public double getAntalEnheder() {
         return antalEnheder;
@@ -13,13 +21,28 @@ public class PN {
      * Registrer datoen for en anvendt dosis.
      */
     public void anvendDosis(LocalDate dato) {
-
-        return false;
+        if (dato.isAfter(super.getStartDato()) && dato.isBefore(super.getSlutDato())) {
+            datoerForAnvendelse.add(dato);
+        }
     }
 
     /** Returner antal gange ordinationen er anvendt. */
     public int antalGangeAnvendt() {
+        return datoerForAnvendelse.size();
+    }
 
-        return -1;
+    @Override
+    public double samletDosis() {
+        return datoerForAnvendelse.size() * antalEnheder;
+    }
+
+    @Override
+    public double døgnDosis() {
+        return samletDosis() / antalDage();
+    }
+
+    @Override
+    public String getType() {
+        return "PN";
     }
 }
