7927acfb500d1afa1a15c95eca291a6bf4faaf43
victor <au670070@uni.au.dk>
2023-10-25 14:07:34 +0200

    dagligfast test af døgndosis

---
+++ test/ordination/DagligFastTest.java
@@ -0,0 +1,40 @@
+package ordination;
+
+import org.junit.jupiter.api.*;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class DagligFastTest {
+    private static Lægemiddel lm;
+    @BeforeEach
+    void setup(){
+        lm = new Lægemiddel("Fucidin", 0.025, 0.025, 0.025, "Styk");
+    }
+
+    @Test
+    void samletDosis() {
+
+    }
+
+    @Test
+    void døgnDosis() {
+        double[] array = {0, 0, 0, 0};
+        DagligFast df = new DagligFast(LocalDate.parse("2019-01-10"),
+                LocalDate.parse("2019-01-12"), array);
+        df.setLægemiddel(lm);
+        assertEquals(df.døgnDosis(), 0);
+
+       array = new double[]{1, 1, 1, 1};
+        df = new DagligFast(LocalDate.parse("2019-01-10"),
+                LocalDate.parse("2019-01-12"), array);
+        assertEquals(df.døgnDosis(), 0.1);
+
+        array = new double[]{0, 2, 0, 1};
+        df = new DagligFast(LocalDate.parse("2019-01-10"),
+                LocalDate.parse("2019-01-12"), array);
+        assertEquals(df.døgnDosis(), 0.075);
+    }
+}
