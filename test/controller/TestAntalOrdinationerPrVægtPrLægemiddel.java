70cf1e08ef89347e82390372008757255014bc82
victor <au670070@uni.au.dk>
2023-10-26 11:15:42 +0200

    victor controller test færdig

--- test/controller/TestAntalOrdinationerPrVægtPrLægemiddel.java
+++ test/controller/TestAntalOrdinationerPrVægtPrLægemiddel.java
@@ -2,6 +2,7 @@ package controller;
 
 import ordination.*;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
@@ -17,20 +18,14 @@ class TestAntalOrdinationerPrVægtPrLægemiddel {
     private LocalDate slutdato2 = LocalDate.parse("2018-12-31");
     private Patient finn;
     private Lægemiddel fucidin = new Lægemiddel("Fucidin", 0.025, 0.025, 0.025, "Styk");
+    private Lægemiddel methorexate = new Lægemiddel(
+            "Methotrexate", 0.01, 0.015,
+            0.02, "Styk");
 
     @BeforeEach
     void setUp() {
-        finn = new Patient("070985-1153", "Finn Madsen", 83.2);
-    }
-
-    @Test
-    void antalOrdinationerPrVægtPrLægemiddel() {
-        //Basisdata
         Controller.setStorage(new Storage());
         Patient ib = Controller.opretPatient("090149-2529", "Ib Hansen", 87.7);
-        Lægemiddel methorexate = Controller.opretLægemiddel(
-                "Methotrexate", 0.01, 0.015,
-                0.02, "Styk");
         fucidin = Controller.opretLægemiddel("Fucidin", 0.025, 0.025, 0.025, "Styk");
         finn = Controller.opretPatient("070985-1153", "Finn Madsen", 83.2);
         Controller.opretPNOrdination(LocalDate.parse("2019-01-20"), LocalDate.parse("2019-01-25"),
@@ -39,41 +34,47 @@ class TestAntalOrdinationerPrVægtPrLægemiddel {
                 finn, fucidin, 2, 0, 1, 0);
         Controller.opretDagligFastOrdination(LocalDate.parse("2019-01-10"), LocalDate.parse("2019-01-15"),
                 finn, fucidin, 0, 0, 1, 0);
+    }
 
-
-        //Testcase 1
+    @Test
+    void testcase1(){
         //Act
         int result = Controller.antalOrdinationerPrVægtPrLægemiddel(75, 100, methorexate);
         //Assert
         assertEquals(result, 0);
-
-        //Testcase 2
+    }
+    @Test
+    void testcase2(){
         //Act
-        result = Controller.antalOrdinationerPrVægtPrLægemiddel(75, 75, methorexate);
+        int result = Controller.antalOrdinationerPrVægtPrLægemiddel(75, 75, methorexate);
         //Assert
         assertEquals(result, 0);
-
-        //Testcase 3
+    }
+    @Test
+    void testcase3(){
         //Act
-        result = Controller.antalOrdinationerPrVægtPrLægemiddel(100, 75, methorexate);
+        int result = Controller.antalOrdinationerPrVægtPrLægemiddel(100, 75, methorexate);
         //Assert
         assertEquals(result, 0);
-
-        //Testcase 4
+    }
+    @Test
+    void testcase4(){
         //Act
-        result = Controller.antalOrdinationerPrVægtPrLægemiddel(75, 100, fucidin);
+        int result = Controller.antalOrdinationerPrVægtPrLægemiddel(75, 100, fucidin);
         //Assert
         assertEquals(result, 3);
-
-        //Testcase 5
+    }
+    @Test
+    void testcase5(){
         //Act
-        result = Controller.antalOrdinationerPrVægtPrLægemiddel(83.2, 83.2, fucidin);
+        int result = Controller.antalOrdinationerPrVægtPrLægemiddel(83.2, 83.2, fucidin);
         //Assert
         assertEquals(result, 2);
-
-        //Testcase 6
+    }
+    @Test
+    void testcase6(){
         //Act
-        result = Controller.antalOrdinationerPrVægtPrLægemiddel(100, 75, fucidin);
+        int result = Controller.antalOrdinationerPrVægtPrLægemiddel(100, 75, fucidin);
         //Assert
         assertEquals(result, 3);
     }
--- test/controller/TestOpretDagligFast.java
+++ test/controller/TestOpretDagligFast.java
@@ -15,17 +15,11 @@ class TestOpretDagligFast {
     private LocalDate startdato = LocalDate.parse("2019-01-01");
     private LocalDate slutdato1 = LocalDate.parse("2019-01-03");
     private LocalDate slutdato2 = LocalDate.parse("2018-12-31");
-    private Patient finn;
+    private Patient finn = new Patient("070985-1153", "Finn Madsen", 83.2);
     private Lægemiddel fucidin = new Lægemiddel("Fucidin", 0.025, 0.025, 0.025, "Styk");
 
-    @BeforeEach
-    void setUp() {
-        finn = new Patient("070985-1153", "Finn Madsen", 83.2);
-    }
-
     @Test
-    void opretDagligFastOrdination() {
-        //Testcase 1
+    void testcase1() {
         //Arrange
         DagligFast df = Controller.opretDagligFastOrdination(startdato, startdato, finn, fucidin, 0, 0, 0, 0);
         //Act
@@ -34,44 +28,53 @@ class TestOpretDagligFast {
         //Assert
         assertEquals(result1, 1);
         assertEquals(result2.toString(), "Fucidin");
+    }
 
-        //Testcase 2
+    @Test
+    void testcase2() {
         //Arrange
-        df = Controller.opretDagligFastOrdination(startdato, slutdato1, finn, fucidin, 0, 0, 0, 0);
+        DagligFast df = Controller.opretDagligFastOrdination(startdato, slutdato1, finn, fucidin, 0, 0, 0, 0);
         //Act
-        result1 = finn.getOrdinationer().size();
-        result2 = df.getLægemiddel();
+        int result1 = finn.getOrdinationer().size();
+        Lægemiddel result2 = df.getLægemiddel();
         //Assert
-        assertEquals(result1, 2);
+        assertEquals(result1, 1);
         assertEquals(result2.toString(), "Fucidin");
+    }
 
-        //Testcase 3
+    @Test
+    void testcase3() {
         //Arrange
-        df = Controller.opretDagligFastOrdination(startdato, slutdato1, finn, fucidin, 1, 2, 0, 4);
+        DagligFast df = Controller.opretDagligFastOrdination(startdato, slutdato1, finn, fucidin, 1, 2, 0, 4);
         //Act
-        result1 = finn.getOrdinationer().size();
-        result2 = df.getLægemiddel();
+        int result1 = finn.getOrdinationer().size();
+        Lægemiddel result2 = df.getLægemiddel();
         //Assert
-        assertEquals(result1, 3);
+        assertEquals(result1, 1);
         assertEquals(result2.toString(), "Fucidin");
+    }
 
-        //Testcase 4
+    @Test
+    void testcase4() {
         //Arrange
-        df = Controller.opretDagligFastOrdination(startdato, slutdato1, finn, fucidin, 1, 2, 3, 0);
+        DagligFast df = Controller.opretDagligFastOrdination(startdato, slutdato1, finn, fucidin, 1, 2, 3, 0);
         //Act
-        result1 = finn.getOrdinationer().size();
-        result2 = df.getLægemiddel();
+        int result1 = finn.getOrdinationer().size();
+        Lægemiddel result2 = df.getLægemiddel();
         //Assert
-        assertEquals(result1, 4);
+        assertEquals(result1, 1);
         assertEquals(result2.toString(), "Fucidin");
+    }
 
+    @Test
+    void testcase5() {
         //Testcase 5
         //Arrange && Assert
         Exception exception = assertThrows(IllegalArgumentException.class, ()-> Controller.opretDagligFastOrdination(startdato, slutdato2, finn, fucidin, 0, 0, 0, 0));
         //Act
-        result1 = finn.getOrdinationer().size();
+        int result1 = finn.getOrdinationer().size();
         //Assert
-        assertEquals(result1, 4);
+        assertEquals(result1, 0);
         assertEquals(exception.getMessage(), "dato forkert");
     }
 }
